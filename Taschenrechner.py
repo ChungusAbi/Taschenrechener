edfe5ebb2efaa87485cdb54b5e475eb420ef8d71
ChungusAbi <107275256+ChungusAbi@users.noreply.github.com>
2022-06-10 21:18:55 +0200

    Create Taschenrechner.py

---
+++ Taschenrechner.py
@@ -0,0 +1,18 @@
+a = int(input("Geben sie bitte ihre Erste Zahl ein : ")
+b = int(input("Geben sie bitte ihre Zweite Zahl ein : ")
+
+z = int(input("1 für + , 2 für -, 3 für *, 4 für / : ")
+
+       
+if z == 1:
+        a + b =c
+        print(c)
+elif z == 2:
+        a - b=c
+        print(c)
+elif z == 3:
+        a * b =c
+        print(c)
+elif z == 4:
+        a / b =c  
+        print(c)
